c280da4c599fabcddc67a1eadb2e8c595b6516e3
Rehan K <RehanK@users.noreply.github.com>
2016-12-09 11:35:05 -0600

    Create Java-Syntex.java

---
+++ Java-Syntex.java
@@ -0,0 +1,23 @@
+       
+       
+       //for loop - will allow you to run certain piece of of code multiple times
+        for(int i = 1; i < 20; i=i+2)
+        {
+			System.out.println("I value is "+i);
+        }
+        
+        //while loop - it will only execute if condition if true otherwise will not run
+        int count = 84;
+        while(count < 100)
+        {
+        	System.out.println("values are " +count);
+        	count++;
+        }
+        
+        //Do while - same as while loop but will execute at least once
+        int ticket = 75;
+        do{
+        	System.out.println("Current ticket number is " +ticket);
+        	ticket++;
+        }while(ticket<=100);
+        	
